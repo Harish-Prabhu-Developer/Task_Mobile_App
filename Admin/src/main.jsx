f11cb2756d8fbecca827b3683e651fd7cc19b2f4
HARISH_ANJAC <23pca121@anjaconline.org>
2025-01-29 23:21:33 +0530

    day 7 OTP1

--- Admin/src/main.jsx
+++ Admin/src/main.jsx
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/Store/index.jsx";
+import store from "./redux/Store/index";
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
